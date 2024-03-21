a08e8269eff70335d4d7ef05a2d90b99a5583608
alukan <kolyaterenya@gmail.com>
2024-03-21 11:41:58 +0100

    added lighthouse for desktop and optimized videos and css

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 - Deleted unnecessary "<" in about.html
-- Added lighthouse `npm run audit`
+- Added lighthouse `npm run audit` for destop and `npm run audit_mobile`
 - To start vite `npm run serve`
 - To build `npm run build`
 - Added bundle-analysis
+
+- Added lazy-loading for video
+- Compressed video
+- Added gzip and Brotli compression
+
+- Added preload for css
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,25 @@
 import { defineConfig } from 'vite';
 import { visualizer } from 'rollup-plugin-visualizer';
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
-  plugins: [
-    visualizer({
-      open: true,
-      sourcemap: true,
-      filename: 'bundle-analysis.html'
-    })
-  ],
+    plugins: [
+        visualizer({
+            open: true,
+            sourcemap: true,
+            gzipSize: true,
+            brotliSize: true,
+            filename: 'bundle-analysis.html',
+
+        }),
+        viteCompression({
+            algorithm: 'brotliCompress',
+            ext: '.br',
+        }),
+        viteCompression({
+            algorithm: 'gzip',
+            ext: '.gz',
+        }),
+
+    ],
 });
